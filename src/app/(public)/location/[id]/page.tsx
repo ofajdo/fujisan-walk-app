f09d3a9552d8d19fb65cd8d7376fa6c509f6eba3
あああああ <kiuchiryou@icloud.com>
2025-08-12 18:44:48 +0900

    error修正

--- src/app/(public)/location/[id]/page.tsx
+++ src/app/(public)/location/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { Detail } from "@/components/location/detail";
 import { LocationSerchById } from "@/data/locations";
 
-export default async function Location({ params }: { params: { id: string } }) {
+export default async function Location({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const location = await LocationSerchById({ id: id });
   return (
