7c108c6170da8acfe6e1ee5b7a3d97dc37871cbf
あああああ <kiuchiryou@icloud.com>
2025-08-24 15:04:21 +0900

    戻るボタンの追加

--- src/app/(public)/location/[id]/page.tsx
+++ src/app/(public)/location/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Detail } from "@/components/location/detail";
 import { LocationSerchById } from "@/data/locations";
+import Link from "next/link";
 
 export default async function Location({
   params,
@@ -9,8 +10,24 @@ export default async function Location({
   const { id } = await params;
   const location = await LocationSerchById({ id: id });
   return (
-    <div className="w-full p-2 bg-gray-100 rounded-xl shadow-md">
-      {location && <Detail location={location} />}
+    <div>
+      <div className="flex gap-2 text-center p-2">
+        <Link
+          className="flex-1 border-2 border-gray-400 rounded-md p-2"
+          href={`/map/${location?.course.id}`}
+        >
+          マップに戻る
+        </Link>
+        <Link
+          className="flex-1 border-2 border-gray-400 rounded-md p-2"
+          href={`/course/${location?.course.id}`}
+        >
+          コース詳細に戻る
+        </Link>
+      </div>
+      <div className="w-full p-2 bg-gray-100 rounded-xl shadow-md">
+        {location && <Detail location={location} />}
+      </div>
     </div>
   );
 }
