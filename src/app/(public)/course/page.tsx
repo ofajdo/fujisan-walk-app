4dc16571f60d31a979d1d734ec7cd15b10e9ed9e
ofajdo <kiuchiryou@icloud.com>
2026-04-07 14:34:09 +0900

    全てのコース追加

--- src/app/(public)/course/page.tsx
+++ src/app/(public)/course/page.tsx
@@ -29,7 +29,7 @@ export const dynamic = "force-static";
 export const metadata: Metadata = {
   title: "コース一覧 - 富士宮市歩く博物館デジタル",
   description:
-    "富士宮市歩く博物館のデジタル版です。富士宮市の歩く博物館の紹介をしています。歩くルートをデジタルのマップで見ることができます。パンフレットとガイドブックを参考にしています",
+    "富士宮市歩く博物館をデジタル化したものです。富士宮市の歩く博物館の紹介をしています。歩くルートをデジタルのマップで見ることができます。パンフレットとガイドブックを参考にしています",
   verification: {
     google: "pN0H3UKaXSIYAoZW9gR1IEyLjFql2k2mErkNYOn9Rbc",
   },
@@ -42,8 +42,7 @@ const Course = async () => {
     <div className="text-center">
       <div className="mb-6">
         <h1 className="text-2xl font-bold mb-2 text-center">コース一覧</h1>
-        <p>（全24コース中 {courses.length}コースのみ）</p>
-        <p>（今後さらに追加する予定です。）</p>
+        <p>（全{courses.length}コース）</p>
       </div>
       <CourseList courses={courses}></CourseList>
       <div>
